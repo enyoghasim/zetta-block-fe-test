2e1ca56d1108d5ea6dc334855086a60d775a365b
Gift <geezyenyoghasim@gmail.com>
2022-09-30 21:31:58 +0100

    added all needed resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,22 +1,53 @@
-import people from "./dataset"; //get all of the available data from our database.
+import fetch from "node-fetch";
+
 const Resolvers = {
   Query: {
-    getAllPeople: () => people, //if the user runs the getAllPeople command
-    //if the user runs the getPerson command:
-    getPerson: (_: any, args: any) => {
-      console.log(args);
-      //get the object that contains the specified ID.
-      return people.find((person) => person.id === args.id);
-    },
-    records: (_: any, args: any) => {
-      console.log(args);
-      return {
-        mentions: ["mention"],
-        emoticons: ["emoticon"],
-        links: ["link"],
+    records: async (_: any, args: any) => {
+      const rawMentions = args.message.match(/@\w+/g);
+
+      // remove @ from mentions
+      const mentions = rawMentions
+        ? rawMentions.map((mention: string) => mention.slice(1))
+        : [];
+
+      // emoticons are alphanumeric characters between parenthesis not longer than 15 characters in length inside parenthesis
+      const emoticonsRaw = args.message.match(/\((\w{1,15})\)/g);
+
+      // remove parenthesis from emoticons
+      const emoticons = emoticonsRaw
+        ? emoticonsRaw.map((emoticon: string) => emoticon.slice(1, -1))
+        : [];
+
+      // all urls/links with http or https protocol or without protocol with www
+      const linksRaw = args.message.match(
+        /((https?:\/\/)|(www\.))[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/g
+      );
+
+      //get the title of the link
+      const getTitles = async (links: string[]): Promise<any> => {
+        return Promise.all(
+          links.map(async (link: string) => {
+            const response = await fetch(link);
+            const text = await response.text();
+
+            const title = text.match(/<title>(.*?)<\/title>/);
+
+            // remove <title> and </title> from title
+            const titleText = title ? title[0].slice(7, -8) : "";
+            return title
+              ? { title: titleText, url: link }
+              : { title: link, url: link };
+          })
+        );
       };
-      //get the object that contains the specified ID.
-      //   return people.find((person) => person.id === args.id);
+
+      const links = await getTitles(linksRaw);
+
+      return {
+        mentions,
+        emoticons,
+        links,
+      };
     },
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,17 +1,15 @@
 import { gql } from "apollo-server-express"; //will create a schema
 const Schema = gql`
-  type Person {
-    id: ID!
-    name: String
+  type Link {
+    title: String
+    url: String
   }
   type Result {
     mentions: [String]
     emoticons: [String]
-    links: [String]
+    links: [Link]
   }
   type Query {
-    getAllPeople: [Person] #will return multiple Person instances
-    getPerson(id: Int): Person
     records(message: String): Result
   }
 `;
